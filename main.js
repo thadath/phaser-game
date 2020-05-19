c42b8eb2ff93d0955f15981ac79a642b86eab338
thadath <thada.chantra@gmail.com>
2020-05-19 14:24:48 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var mainState = {
+	preload: function() {
+
+	},
+
+	create: function() {
+        game.stage.backgroundColor = '#e26a6b';
+	},
+
+	update: function() {
+
+	}
+};
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+
+game.state.add('main', mainState);
+game.state.start('main');
+

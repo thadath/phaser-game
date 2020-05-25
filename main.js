02e8898b00c4b9ee0a894897a6348bab2e7a6d6b
thadath <thada.chantra@gmail.com>
2020-05-25 14:04:29 +0700

    third commit

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var mainState = {
         game.load.image('devahoy', './assets/logo.png');
         game.load.image('ground', 'assets/ground.png');
         game.load.spritesheet('player', 'assets/warrior_m.png', 32, 36);
+        game.load.spritesheet('coin', 'assets/coins.png', 40, 44, 4);
     },
+    
 
     create: function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -12,32 +14,92 @@ var mainState = {
 
         this.myWorld.enableBody = true;
         var ground = this.myWorld.create(0, game.world.height - 64, 'ground');
-        ground.scale.setTo(2, 2);
+        ground.scale.setTo(4, 2);
         ground.body.immovable = true;
-        var left = this.myWorld.create(0, 450 - 32, 'ground');
+        var left = this.myWorld.create(40, 550 - 32, 'ground');
         left.body.immovable = true;
 
-        var right = this.myWorld.create(450, 350 - 32, 'ground');
+        var right = this.myWorld.create(600, 650 - 32, 'ground');
         right.body.immovable = true;
 
-        var middle = this.myWorld.create(250, 250 - 32, 'ground');
+        var middle = this.myWorld.create(550, 450 - 32, 'ground');
         middle.scale.setTo(0.3, 1);
         middle.body.immovable = true;
 
-        var top = this.myWorld.create(100, 150 -32, 'ground');
-        top.scale.setTo(0.2, 1);
+        var top = this.myWorld.create(800, 350 -32, 'ground');
+        top.scale.setTo(0.3, 1);
         top.body.immovable = true;
         game.stage.backgroundColor = '#e26a6b';
         game.add.sprite(0, 0, 'devahoy').anchor.setTo(0, 0); 
+
+        // this.player = game.add.sprite(0, 700, 'player');
+        // game.physics.arcade.enable(this.player);
+
+        // this.player.body.bounce.y = 0.25;
+        // this.player.body.gravity.y = 980;
+        // this.player.body.collideWorldBounds = true;
+
+        // this.player.animations.add('right', [3, 4, 5], 10, true);
+        // this.player.animations.add('left', [9, 10, 11], 10, true);
+
+        // this.player.frame = 6;
+
+        // this.cursors = this.input.keyboard.createCursorKeys();
+
+        // this.coins = game.add.group();
+        // this.coins.enableBody = true;
+        // this.spawnCoins();
+
     },
 
 	update: function() {
+        game.physics.arcade.collide(this.player, this.myWorld);
+        game.physics.arcade.enable(this.player);
 
-	}
-};
+        if (this.cursors.right.isDown) {
+
+        }
+        if (this.cursors.left.isDown) {
+
+        }
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.body.velocity.y = -500;
+        }
+
+        this.player.body.velocity.x = 0;
+        if (this.cursors.right.isDown) {
+        this.player.body.velocity.x = 200;
+        this.player.animations.play('right');
+        } else if (this.cursors.left.isDown) {
+        this.player.body.velocity.x = -200;
+        this.player.animations.play('left');
+        } else {
+        this.player.animations.stop();
+        this.player.frame = 6;
+        this.player.body.touching.down
+        }  
+    },
+
+    spawnCoins: function() {
+        for (var i = 0; i<10;i++) {
+            var x = this.rnd.integerInRange(0, game.world.width - 40);
+            var y = this.rnd.integerInRange(0, game.world.height - 100);
+
+            this.conis.create(x,y,'coin');
+            this.coins.forEach(function(coin) {
+                coin.animations.add('effect', [0, 1, 2, 3], 5, true);
+                coin.animations.play('effect');
+                coin.scale.setTo(0.5, 0.5);
+                coin.body.gravity.y = 200;
+                coin.body.bounce.y = 0.5;
+            });
+        }
+    }
+}
 
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+
+var game = new Phaser.Game(1600, 800, Phaser.AUTO, 'game');
 
 game.state.add('main', mainState);
 game.state.start('main');
